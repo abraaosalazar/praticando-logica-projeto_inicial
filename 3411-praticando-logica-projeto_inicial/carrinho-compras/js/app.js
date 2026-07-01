d2657949a9e06b808a1101a6541e93473530aa24
abraaosalazar <abraaosalazar@gmail.com>
2026-07-01 18:42:51 -0300

    update

--- 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,11 +1,27 @@
-let carrinho = [];
+let produtosCarrinho = [];
+let valorUnitário = [];
+let valorTotal = 0
+
 function adicionar(){
     let produto = document.getElementById('produto').value;
-    carrinho.push(produto);
-    console.log(carrinho);
+    let nomeProduto = produto.split('-')[0];
+    let valorProtuto = parseInt(produto.split('R$')[1]);
+    let quantidade = document.getElementById('quantidade').value;
+
+    for (let index = 0; index < quantidade; index++) {
+            produtosCarrinho.push(nomeProduto);
+            valorTotal = valorTotal + valorProtuto;
+
+    }
+    document.getElementById('valor-total').innerHTML = `R$ ${valorTotal}`;
+    console.log(produtosCarrinho);
+    console.log(valorUnitário);
+    console.log(valorTotal);
 }
 
 function limpar(){
-    carrinho = [];
-    console.log(carrinho);
+    valorTotal = 0
+    document.getElementById('valor-total').innerHTML = `R$ ${valorTotal}`;
+    produtosCarrinho = [];
+    console.log(produtosCarrinho);
 }
