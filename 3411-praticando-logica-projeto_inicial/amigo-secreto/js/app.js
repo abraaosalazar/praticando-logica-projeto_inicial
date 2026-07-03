efea5a26706beb2e65f85e99439f6c3ec8fbffeb
abraaosalazar <abraaosalazar@gmail.com>
2026-07-03 15:36:11 -0300

    att

--- 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,29 +1,44 @@
 let participantes = [];
-let inputNome = document.getElementById('nome-amigo');
+let participante = document.getElementById('nome-amigo');
 
 function adicionar(){
-    if(inputNome.value != ''){
-        nomeParticipante = inputNome.value;
-        participantes.push(nomeParticipante)
-        console.log(participantes);
-        inputNome.value = '';
-        document.getElementById('lista-amigos').innerHTML = participantes;
+    if(participante.value != ''){
+        let lista = document.getElementById('lista-amigos');
+        participantes.push(participante.value);
+
+        if(lista.textContent == ''){
+            lista.textContent = participante.value;
+        } else{
+            lista.textContent = lista.textContent + ', ' + participante.value;
+        }
+
+        participante.value = '';
+
     } else{
         return
     }
 }
 
 function sortear(){
-    let sorteados = participantes;
     
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
+    for (let index = 0; index < participantes.length; index++) {
+        let idPaticipante = parseInt(Math.random()* participantes.length);
+        let idSorteado = parseInt(Math.random()* sorteados.length);
+
+        while (idPaticipante == idSorteado) {
+            idSorteado = parseInt(Math.random()* sorteados.length)
+        }
+
+        console.log(`Participante: ${participantes[idPaticipante]}, Sorteado: ${sorteados[idSorteado]}`);
+
+        participantes.splice(idPaticipante, 1);
+        sorteados.splice(idSorteado, 1);
         
     }
 }
 
 function reiniciar(){
     participantes = [];
-    document.getElementById('lista-amigos').innerHTML = participantes;
+    lista.textContent = '';
 }
 
