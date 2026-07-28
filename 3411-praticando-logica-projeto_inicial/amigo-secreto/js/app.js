620b558e9ecaa93c235db52be6ea1a4de7de7f7e
abraaosalazar <abraaosalazar@gmail.com>
2026-07-28 17:43:33 -0300

    Update app.js

--- 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ 3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,9 +1,9 @@
 let participantes = [];
 let participante = document.getElementById('nome-amigo');
+let lista = document.getElementById('lista-amigos');
 
 function adicionar(){
     if(participante.value != ''){
-        let lista = document.getElementById('lista-amigos');
         participantes.push(participante.value);
 
         if(lista.textContent == ''){
@@ -20,22 +20,26 @@ function adicionar(){
 }
 
 function sortear(){
-    
+    embaralha(participantes);
+    let sorteio = document.getElementById('lista-sorteio');
     for (let index = 0; index < participantes.length; index++) {
-        let idPaticipante = parseInt(Math.random()* participantes.length);
-        let idSorteado = parseInt(Math.random()* sorteados.length);
-
-        while (idPaticipante == idSorteado) {
-            idSorteado = parseInt(Math.random()* sorteados.length)
-        }
-
-        console.log(`Participante: ${participantes[idPaticipante]}, Sorteado: ${sorteados[idSorteado]}`);
-
-        participantes.splice(idPaticipante, 1);
-        sorteados.splice(idSorteado, 1);
-        
+        sorteio.innerHTML = sorteio.innerHTML + participantes[index] + '-->' + participantes[index + 1] + '<br>';
     }
 }
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+
 
 function reiniciar(){
     participantes = [];
